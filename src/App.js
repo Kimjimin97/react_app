c213bd5cc7ad3c60a80ed93b35e691ff40274f18
Kimjimin97 <sja9707@daum.net>
2022-07-03 22:18:52 +0900

    button useeffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { useState } from "react";
 import Button from "./Button.js";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("render");
   return (
     <div>
       <h1>Welcome back!</h1>
-      <Button text={"content"} />
+      <div>{counter}</div>
+      {console.log(counter)}
+      <button onClick={onClick}> click me</button>
     </div>
   );
 }
